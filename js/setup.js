ee1242020ec2b17c7c6628dc1f7bb6ef579e9eef
XeniaTkacheva <xenia1898@gmail.com>
2018-11-29 09:23:22 +0100

    Добавляет функции генерации объекта волшебников и JS элементов

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var WIZARDS_COUNT = 4;
+
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
@@ -8,10 +10,14 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
   .content
   .querySelector('.setup-similar-item');
 
-var firstNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var lastNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var colorToEyes = ['black', 'red', 'blue', 'yellow', 'green'];
-var colorToCoat = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+// Исходные данные для создания волшебников
+
+var wizardsData = {
+  firstNames: ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'],
+  lastNames: ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'],
+  colorToEyes: ['black', 'red', 'blue', 'yellow', 'green'],
+  colorToCoat: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)']
+};
 
 // Функция генерации случайного целого числа
 
@@ -19,17 +25,45 @@ var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 };
 
+// Функция генерации объекта волшебников
+
+var makeWisards = function (data) {
+
+  var wizards = [];
+
+  for (var j = 0; j < WIZARDS_COUNT; j++) {
+
+    var wizard = {
+      name: data.firstNames[getRandomInt(0, (data.firstNames.length - 1))] + ' ' + data.lastNames[getRandomInt(0, (data.lastNames.length - 1))],
+      coatColor: data.colorToCoat[getRandomInt(0, (data.colorToCoat.length - 1))],
+      eyesColor: data.colorToEyes[getRandomInt(0, (data.colorToEyes.length - 1))]
+    };
+    wizards.push(wizard);
+  }
+  return wizards;
+};
+
+// Создаем объект волшебников
+
+var wizards = makeWisards(wizardsData);
+
 var fragment = document.createDocumentFragment();
 
-for (var i = 0; i < 4; i++) {
+// Функция создания JS элемента
+
+var renderWizard = function (wizardsArray) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
-  wizardElement.querySelector('.setup-similar-label').textContent = firstNames[getRandomInt(0, (firstNames.length - 1))] + ' ' + lastNames[getRandomInt(0, (lastNames.length - 1))];
-  wizardElement.querySelector('.wizard-coat').style.fill = colorToCoat[getRandomInt(0, (colorToCoat.length - 1))];
-  wizardElement.querySelector('.wizard-eyes').style.fill = colorToEyes[getRandomInt(0, (colorToEyes.length - 1))];
+  wizardElement.querySelector('.setup-similar-label').textContent = wizardsArray.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizardsArray.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizardsArray.eyesColor;
+  return wizardElement;
+};
 
-  fragment.appendChild(wizardElement);
+for (var i = 0; i < 4; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
 }
+
 similarListElement.appendChild(fragment);
 
 document.querySelector('.setup-similar').classList.remove('hidden');
